c4be8f4f81e57c089fec80f9716e1dbacbfcce45
atomicweb <steliospanag2012@gmail.com>
2024-10-04 21:33:36 +0300

    no

---
+++ scripts/splash.js
@@ -0,0 +1,15 @@
+var splashes = [
+    "DOOM!",
+    "100% more BANG!",
+    "Quake!",
+    "No Trillinear filtering!",
+    "Supply and demand!",
+    "Fantastic!",
+    "May contain nuts!",
+    "Check out Terraria!",
+    "Check out Factorio!",
+    "Check out Half Life!",
+    "Check out AoE!",
+    "Check out Wolfenstein 3D!"
+]
+document.body.innerHTML = splashes.random;
