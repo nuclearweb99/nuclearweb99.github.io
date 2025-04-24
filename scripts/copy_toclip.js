f0abc89dc241e5df185749ab85b30ae7fafdb164
nuclearweb99 <steliospanag2012@gmail.com>
2025-04-24 22:39:38 +0300

    Slightly updated partacc

--- scripts/copy_toclip.js
+++
@@ -1,4 +0,0 @@
-function copyText() {
-    var copyText = "<embed type='text/html' src='nuclearweb99.github.io/games/partacc/index.html' width='500' height='200'>";
-    navigator.clipboard.writeText(copyText);
-  }
