beae9395857ae447b8588469d94bc73e831367cd
nuclearweb99 <steliospanag2012@gmail.com>
2025-04-24 22:28:20 +0300

    Fixed like everything

---
+++ scripts/copy_toclip.js
@@ -0,0 +1,4 @@
+function copyText() {
+    var copyText = "<embed type='text/html' src='nuclearweb99.github.io/games/partacc/index.html' width='500' height='200'>";
+    navigator.clipboard.writeText(copyText);
+  }
